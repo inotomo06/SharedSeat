f3d47f37ebc4efaa6144e0d7cf4f967396bdc312
inotomo06 <86100981+inotomo06@users.noreply.github.com>
2022-09-27 11:44:17 +0900

    Initial Commit

---
+++ SharedSeat/SharedSeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedSeatApp.swift
+//  SharedSeat
+//
+//  Created by cmStudent on 2022/09/27.
+//
+
+import SwiftUI
+
+@main
+struct SharedSeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
